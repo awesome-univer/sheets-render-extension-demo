3bf470c534e3e0d7ff69ca1797652baaa475df87
Dushusir <1414556676@qq.com>
2024-11-29 15:08:10 +0800

    fix: update version, use presets

--- src/main.ts
+++ src/main.ts
@@ -1,67 +1,36 @@
-import "./style.css";
-import "@univerjs/design/lib/index.css";
-import "@univerjs/ui/lib/index.css";
-import "@univerjs/sheets-ui/lib/index.css";
-import "@univerjs/sheets-formula/lib/index.css";
-import "@univerjs/sheets-numfmt/lib/index.css";
+import { createUniver, defaultTheme, LocaleType, Tools } from '@univerjs/presets'
+import { UniverSheetsCorePreset } from '@univerjs/presets/preset-sheets-core'
+import sheetsCoreEnUS from '@univerjs/presets/preset-sheets-core/locales/en-US'
 
-import { LocaleType, Univer } from "@univerjs/core";
-import { defaultTheme } from "@univerjs/design";
-import { UniverDocsPlugin } from "@univerjs/docs";
-import { UniverDocsUIPlugin } from "@univerjs/docs-ui";
-import { UniverFormulaEnginePlugin } from "@univerjs/engine-formula";
-import { UniverRenderEnginePlugin } from "@univerjs/engine-render";
-import { UniverSheetsPlugin } from "@univerjs/sheets";
-import { UniverSheetsFormulaPlugin } from "@univerjs/sheets-formula";
-import { UniverSheetsNumfmtPlugin } from "@univerjs/sheets-numfmt";
-import { UniverSheetsUIPlugin } from "@univerjs/sheets-ui";
-import { UniverUIPlugin } from "@univerjs/ui";
-import { FUniver } from '@univerjs/facade';
-import { zhCN, enUS } from 'univer:locales'
-import { RowHeaderCustomExtension } from "./row-header-extension";
-import { ColumnHeaderCustomExtension } from "./column-header-extension";
-import { MainCustomExtension } from "./main-extension";
+import './style.css'
+import '@univerjs/presets/lib/styles/preset-sheets-core.css'
+import { RowHeaderCustomExtension } from './row-header-extension'
+import { ColumnHeaderCustomExtension } from './column-header-extension'
+import { MainCustomExtension } from './main-extension'
 
-// univer
-const univer = new Univer({
-  theme: defaultTheme,
+const { univerAPI } = createUniver({
   locale: LocaleType.EN_US,
   locales: {
-    [LocaleType.ZH_CN]: zhCN,
-    [LocaleType.EN_US]: enUS,
+    enUS: Tools.deepMerge(
+      {},
+      sheetsCoreEnUS,
+    ),
   },
-});
-
-// core plugins
-univer.registerPlugin(UniverDocsPlugin, {
-  hasScroll: false,
-});
-univer.registerPlugin(UniverDocsUIPlugin);
-univer.registerPlugin(UniverRenderEnginePlugin);
-univer.registerPlugin(UniverUIPlugin, {
-  container: "app",
-  header: true,
-  toolbar: true,
-  footer: true,
-});
-univer.registerPlugin(UniverSheetsPlugin);
-univer.registerPlugin(UniverSheetsUIPlugin);
-
-// sheet feature plugins
-univer.registerPlugin(UniverSheetsNumfmtPlugin);
-univer.registerPlugin(UniverFormulaEnginePlugin);
-univer.registerPlugin(UniverSheetsFormulaPlugin);
+  theme: defaultTheme,
+  presets: [
+    UniverSheetsCorePreset(),
+  ],
+})
 
 const unitId = 'workbook'
+
 // create univer sheet instance
-univer.createUniverSheet({id:unitId,sheets:{
+univerAPI.createUniverSheet({id:unitId,sheets:{
   'sheet-01':{
     id:'sheet-01',
     name:'Sheet01'
   }
 }});
-
-const univerAPI = FUniver.newAPI(univer);
 
 // register custom extension
 univerAPI.getHooks().onRendered(() => {
